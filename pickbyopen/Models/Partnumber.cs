d7f6011bc2936c0f2712340a201347b1609b8d48
Cristiano Leite <cristianosleite@hotmail.com>
2024-11-09 04:40:16 -0300

    Fix: type must be a long

--- pickbyopen/Models/Partnumber.cs
+++ pickbyopen/Models/Partnumber.cs
@@ -1,8 +1,10 @@
 ﻿namespace Pickbyopen.Models
 {
-    public class Partnumber(string partnumber, string description)
+    public class Partnumber(long id, string partnumber, string description)
     {
+        public long PartnumberId { get; set; } = id;
         public string Code { get; set; } = partnumber;
         public string Description { get; set; } = description;
+        public List<RecipePartnumber> RecipePartnumbers { get; set; } = [];
     }
 }
